001ec00079034403b721dccb1f683eda0433361d
AndreCeridian <AndreCeridian@users.noreply.github.com>
2022-09-15 19:19:25 +0000

    deploy: 3cfd8ff3931af9554539b5d54600c3917f9d3d4b

---
+++ hierarchy.js
@@ -0,0 +1,7 @@
+var hierarchy =
+[
+    [ "MyLibrary::Example", "classMyLibrary_1_1Example.html", [
+      [ "MyLibrary::SubclassExample< TemplatedClass >", "classMyLibrary_1_1SubclassExample.html", null ]
+    ] ],
+    [ "HelloWorld.Program", "classHelloWorld_1_1Program.html", null ]
+];
