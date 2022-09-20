af6e7aaa4ae0d785599c3fbc6937edf7d2c61871
AndreCeridian <108750695+AndreCeridian@users.noreply.github.com>
2022-09-20 09:30:06 -0500

    update

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,9 +4,15 @@ namespace HelloWorld
 {
     class Program
     {
+        //comment
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Again, World!");
         }
+        
+        public static void MethodTest() {
+            //test comment
+            Console.WriteLine("Another method test");
+        {
     }
 }
