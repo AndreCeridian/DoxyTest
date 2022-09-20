38cc9ad3facbe0757d97877c0647f85d68d29c0d
AndreCeridian <AndreCeridian@users.noreply.github.com>
2022-09-20 14:38:09 +0000

    deploy: ea7f1b16f71bc82a1bcbb9b36936b9b28db25029

--- annotated_dup.js
+++ annotated_dup.js
@@ -1,7 +1,7 @@
 var annotated_dup =
 [
     [ "HelloWorld", "namespaceHelloWorld.html", [
-      [ "TestClass1", "classHelloWorld_1_1TestClass1.html", null ]
+      [ "TestClass1", "classHelloWorld_1_1TestClass1.html", "classHelloWorld_1_1TestClass1" ]
     ] ],
     [ "MyLibrary", null, [
       [ "Example", "classMyLibrary_1_1Example.html", "classMyLibrary_1_1Example" ],
---
+++ classHelloWorld_1_1TestClass1.js
@@ -0,0 +1,5 @@
+var classHelloWorld_1_1TestClass1 =
+[
+    [ "TestFuction", "classHelloWorld_1_1TestClass1.html#a3fd8f9a4b2f85c2045daa89a2586eb3d", null ],
+    [ "TestFuction2", "classHelloWorld_1_1TestClass1.html#a2f008528e4d93a7a3d037b359aa9b607", null ]
+];
