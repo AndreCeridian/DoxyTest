6c059408e6c76d6adbe054d22b40c10b2fca12f0
AndreCeridian <108750695+AndreCeridian@users.noreply.github.com>
2022-09-20 09:31:33 -0500

    add new class

---
+++ HelloWorld/TestClass1.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace HelloWorld
+{
+    class TestClass1
+    {
+        //comment
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Again, World!");
+        }
+    }
+}
